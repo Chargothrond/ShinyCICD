425989135debd842e9bd61e393f33d44398d3f7f
Peter Schmid <peter.schmid@mirai-solutions.com>
2020-06-12 17:31:14 +0200

    do demo issue

--- R/app_server.R
+++ R/app_server.R
@@ -8,11 +8,19 @@
 app_server <- function(input, output, session) {
 
   x <- faithful[, 2]
+  y <- faithful[, 1]
+
   bins <- reactive(seq(min(x), max(x), length.out = input$bins + 1))
+  binsy <- reactive(seq(min(y), max(y), length.out = input$bins + 1))
 
   output$distPlot <- renderPlot({
     # draw the histogram with the specified number of bins
     hist(x, breaks = bins(), col = 'darkgray', border = 'white')
+  })
+
+  output$distPlot2 <- renderPlot({
+    # draw the histogram with the specified number of bins
+    hist(y, breaks = binsy(), col = 'darkgray', border = 'white')
   })
 
   output$table <- DT::renderDT(
